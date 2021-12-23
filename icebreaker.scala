db6a9e16e96f1edb6bb792c385d29eeeba2c9028
Min4649 <55066626+Min4649@users.noreply.github.com>
2021-12-23 15:30:05 -0500

    Update icebreaker.scala

--- icebreaker.scala
+++ icebreaker.scala
@@ -20,15 +20,16 @@ class Lattice extends FPGA:
     //TODO: fill pin by board image
 
 class Icebreaker extends Board:
-    val pmod = Pmod()
-    val fpga = lattice()
+    val pmod1A = Pmod1A()
+    val pmod1B = Pmod1B()
+    val fpga = Lattice()
     val app = IntergratingApp()
 
-    fpga.p42() <> pmod.P1B7()
-    fpga.p36() <> pmod.P1B8()
-    fpga.p45() <> pmod.P1A4()
-    fpga.p31() <> pmod.P1B4()
-    fpga.p44() <> pmod.P1A10()
+    fpga.p42() <> pmod1B.P1B7()
+    fpga.p36() <> pmod1B.P1B8()
+    fpga.p45() <> pmod1A.P1A4()
+    fpga.p31() <> pmod1B.P1B4()
+    fpga.p44() <> pmod1A.P1A10()
 
 class VgaPmod extends Board:
     val pmod = Pmod()
@@ -45,12 +46,14 @@ class ConnectedBoard extends Board:
     val vgapmod = VgaPmod()
     icebreaker.pmod <> vgapmod.pmod
 
-class Pmod extends Interface:
+class Pmod1A extends Interface:
+    val P1A4 = Pin()
+    val P1A10 = Pin()
+
+class Pmod1B extends Interface:
     val P1B7 = Pin()
     val P1B8 = Pin()
-    val P1A4 = Pin()
     val P1B4 = Pin()
-    val P1A10 = Pin()
 
 class IntergratingApp(app : ProjectFApp):
     def build : Unit ={}
