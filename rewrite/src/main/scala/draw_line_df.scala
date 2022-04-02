c0a4fe4e07b9f12cdd34efe008858b755689db38
Min4649 <55066626+Min4649@users.noreply.github.com>
2022-04-01 22:25:08 -0400

    Update draw_line_df.scala

--- rewrite/src/main/scala/draw_line_df.scala
+++ rewrite/src/main/scala/draw_line_df.scala
@@ -7,37 +7,29 @@ class draw_line_df(
   // val clk = DFBit <> IN
   val start = DFBool <> IN
   val oe = DFBool <> IN
-  val x0 = DFSInt(CORDW) <> IN
-  val y0 = DFSInt(CORDW) <> IN
-  val x1 = DFSInt(CORDW) <> IN
-  val y1 = DFSInt(CORDW) <> IN
-  val x = DFSInt(CORDW) <> OUT init 0
-  val y = DFSInt(CORDW) <> OUT init 0
+  object rectDefs extends RectDefs(CORDW)
+  val diagCoord_in = rectDefs.DiagnolCoord <> IN
+
+  object videoDefs extends VideoDefs(CORDW)
+  val coord = videoDefs.Coord <> OUT init videoDefs.Coord(0,0)
   val drawing = DFBool <> OUT
   val busy = DFBool <> OUT init 0
   val done = DFBool <> OUT init 0
 
   val swap = DFBool <> VAR
   val right = DFBool <> VAR
-  val xa = DFSInt(CORDW) <> VAR init 0
-  val ya = DFSInt(CORDW) <> VAR init 0
-  val xb = DFSInt(CORDW) <> VAR init 0
-  val yb = DFSInt(CORDW) <> VAR init 0
-  val x_end = DFSInt(CORDW) <> VAR
-  val y_end = DFSInt(CORDW) <> VAR
-
-  swap := y0 > y1
+  val diagCoord = rectDefs.DiagnolCoord <> VAR init ?
+  val endCoord = videoDefs.Coord <> VAR
+  import rectDefs.swapF
+  swap := diagCoord_in.y0 > diagCoord_in.y1
   if (swap)
-    xa := x1
-    xb := x0
-    ya := y1
-    yb := y0
+    diagCoord := diagCoord_in.swapF
+    // diagCoord.x0 := diagCoord_in.x1
+    // diagCoord.x1 := diagCoord_in.x0
+    // diagCoord.y0 := diagCoord_in.y1
+    // diagCoord.y1 := diagCoord_in.y0
   else
-    xa := x0
-    xb := x1
-    ya := y0
-    yb := y1
-
+    diagCoord := diagCoord_in
   val err = DFSInt(CORDW+1) <> VAR init 0
   val dx = DFSInt(CORDW+1) <> VAR init 0
   val dy = DFSInt(CORDW+1) <> VAR init 0
@@ -74,46 +66,46 @@ class draw_line_df(
   state match
     case DRAW() => 
       if (oe) 
-        if (x == x_end && y == y_end) 
+        if (coord == endCoord) 
           busy := 0
           done := 1
         else 
           if (movx) 
             if (right)
-              x := x.prev(1) + 1
+              coord.x := coord.x.prev(1) + 1
             else
-              x := x.prev(1) - 1
+              coord.x := coord.x.prev(1) - 1
           err := err.prev(1) + dy.prev(1)
 
           if (movy) 
-            y := y.prev(1) + 1
+            coord.y := coord.y.prev(1) + 1
             err := err.prev(1) + dx.prev(1)
 
           if (movx && movy) 
             if (right)
-              x := x.prev(1) + 1
+              coord.x := coord.x.prev(1) + 1
             else
-              x := x.prev(1) - 1
-            y := y.prev(1) + 1
+              coord.x := coord.x.prev(1) - 1
+            coord.y := coord.y.prev(1) + 1
             err := err.prev(1) + dy.prev(1) + dx.prev(1)
     case INIT_0() => 
       if (right)
-        dx := xb.prev(1) - xa.prev(1)
+        dx := diagCoord.x1.prev(1) - diagCoord.x0.prev(1)
       else
-        dx := xa.prev(1) - xb.prev(1)
-      dy := ya.prev(1) - yb.prev(1)
+        dx := diagCoord.x0.prev(1) - diagCoord.x1.prev(1)
+      dy := diagCoord.y0.prev(1) - diagCoord.y1.prev(1)
     case INIT_1() => 
       err := dx.prev(1) + dy.prev(1)
-      x := xa.prev(1)
-      y := ya.prev(1)
-      x_end := xb.prev(1)
-      y_end := yb.prev(1)
+      coord.x := diagCoord.x0.prev(1)
+      coord.y := diagCoord.y0.prev(1)
+      endCoord.x := diagCoord.x1.prev(1)
+      endCoord.y := diagCoord.y1.prev(1)
     case _ => 
       done := false
       if (start)
         err := dx.prev(1) + dy.prev(1)
         busy := true
-        right := xa.prev(1) < xb.prev(1)
+        right := diagCoord.x0.prev(1) < diagCoord.x1.prev(1)
       
 // @main def hello: Unit = 
 //   import DFiant.compiler.stages.printCodeString
